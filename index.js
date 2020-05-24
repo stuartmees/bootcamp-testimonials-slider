4eb75430d342894a25f6b93d1e51c85d4d78da0a
Stuart Mees <stuartmees@gmail.com>
2020-05-24 17:13:57 +0100

    mobile break point added

--- index.js
+++ index.js
@@ -26,16 +26,16 @@ window.addEventListener('DOMContentLoaded', function(){
 
     function popuplateTestimonial() {
         const innerHTML =     
-        `<figure class="fade-in">
-            <img class="image" src=${testimonials[i].image} width='400' heigh='200'>
-            <figcaption>
-                <p class="text">" ${testimonials[i].text} "</p>
-                <div>
-                    <span class="name">${testimonials[i].name}</span>
-                    <span class="role">${testimonials[i].role}</span>
-                <div>
-            </figcaption>
-        </figure>`
+            `<figure class="fade-in">
+                <img class="image" src=${testimonials[i].image} width='400' heigh='200'>
+                <figcaption>
+                    <p class="text">" ${testimonials[i].text} "</p>
+                    <div>
+                        <span class="name">${testimonials[i].name}</span>
+                        <span class="role">${testimonials[i].role}</span>
+                    <div>
+                </figcaption>
+            </figure>`
         
         testimonial.innerHTML = innerHTML;
     };
