37761133565b18f69b493158e22eacdc1bb59179
Stuart Mees <stuartmees@gmail.com>
2020-05-25 15:40:34 +0100

    prior to splitting article

--- js/components/testimonial.js
+++ js/components/testimonial.js
@@ -3,8 +3,11 @@
     const innerHTML = (testimonials, i) => {
         return(
             `<article class="fade-in">
-                <img class="image" src=${testimonials[i].image} width='400' heigh='200'>
-                <section class="caption">
+                <section class="photo">
+                    <img class="image" src=${testimonials[i].image}>
+                </section>
+
+                <section class="quote">
                     <p class="text">" ${testimonials[i].text} "</p>
                     <div>
                         <span class="name">${testimonials[i].name}</span>
